45b94462b6f284bb38856d512c7734c760598b8d
zeynepsen <zszynpsen@gmail.com>
2020-10-23 19:47:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const testVar = {}
+/* const testVar = {}
 
 function testFunc() {
   return "hi"
-}
+} */
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin(myArray) {
+  const myObject = myArray.find(object => { 
+    return object.result === "W"
+  })
+  
+  if (myObject) {
+    return myObject.year
+  } else undefined
+  }
